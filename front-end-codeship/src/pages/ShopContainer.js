39665dd1167af13549af4209d7e3d40c9965b1ea
Carlota Gallart <carlota.gallart@gmail.com>
2020-11-23 19:11:14 +0100

    ShopContainer page

---
+++ front-end-codeship/src/pages/ShopContainer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// import components
+import ProductCard from '../components/ProductCard'
+import ShoppingCart from '../components/ShoppingCart'
+import TopSpaceShip from '../components/TopSpaceShip'
+function ShopContainer(){
+return(
+<div>ShopContainer
+<ProductCard></ProductCard>
+<TopSpaceShip></TopSpaceShip>
+<ShoppingCart></ShoppingCart>
+</div>
+
+)
+}
+export default ShopContainer;
