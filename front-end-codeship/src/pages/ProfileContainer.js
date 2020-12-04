3ca2ab521afaec56ff1943f3637d7c688f6f9b65
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-05 00:17:59 +0100

    profile structure

---
+++ front-end-codeship/src/pages/ProfileContainer.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+//Profile Components 
+import Slider from "../components/Slider"
+
+function ProfileContainer(){
+    return(
+  <div className="profile-container">
+      <div className="profile-content">
+          <div className="sidenav">
+              <div className="points-content">
+
+              </div>
+              <div className="navbar">
+
+            </div>
+
+          </div>
+          <div className="profile-info-container">
+              <div className="users-container">
+
+              </div>
+              <div className="profile-spaceship-info">
+                  <div className="profile-spaceship-details">
+
+                  </div>
+                  <div className="user-spaceship-details">
+                      <div className="user-spaceship-link-container">
+
+                      </div>
+                      <div className="user-spaceship-container">
+                          <div className="image-container">
+
+                          </div>
+
+                      </div>
+                      <div className="products-slider-container">
+
+                      </div>
+                      
+                  </div>
+
+              </div>
+
+          </div>
+
+      </div>
+  </div>
+    )
+    }
+    export default ProfileContainer;
