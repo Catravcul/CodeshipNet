d0817eafb109d7854e843d7f0540df735c1a5ccf
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-04 23:16:18 +0100

    register structure

---
+++ front-end-codeship/src/pages/RegisterContainer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Register Components
+import Form from "../components/Form"
+
+function RegisterContainer(){
+    return(
+            <div className="register-container">
+                    <Form></Form>
+            </div>
+    )
+}
+
+export default RegisterContainer;
+
