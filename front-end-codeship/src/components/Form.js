870d3858c2cbcc121e968dca5e6270734ead3728
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-04 23:19:09 +0100

    Register Form Component - structure only

---
+++ front-end-codeship/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Form() {
+
+  return (
+        <div className="form-container">
+            <div className="actual-form">
+            <div className="image-register-container"></div>
+            <div className="form-register-container"></div>
+            </div>
+        </div>
+  );
+}
+
+export default Form;
