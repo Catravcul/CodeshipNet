9d7c26b5c1c57dbf53451e043468deabe17c2eaf
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-06 00:59:45 +0100

    nav buttons component

---
+++ front-end-codeship/src/components/ButtonsNav.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+function NavButton(){
+    return(
+    <div className="nav-buttons-container">
+            <button type="button"> Profile </button>
+    </div>)
+}
+
+export default NavButton;
