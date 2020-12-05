6532fc2d79f6e5ba1be3208862ddf1252387a4fc
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-06 00:33:56 +0100

    get images for slider from api

--- front-end-codeship/src/components/Slider.js
+++ front-end-codeship/src/components/Slider.js
@@ -1,40 +1,44 @@
 import React, {Component} from "react";
 import Carousel from 'react-elastic-carousel';
 import Item from "./Item";
+import axios from "axios";
 
 const breakPoints = [
-    { width: 1, itemsToShow: 8 },
-    { width: 550, itemsToShow: 2, itemsToScroll: 2 },
-    { width: 768, itemsToShow: 3 },
-    { width: 1200, itemsToShow: 4 }
+    { width: 1, itemsToShow: 8 }
+    // { width: 3, itemsToShow: 2, itemsToScroll: 2 },
+    // { width: 4, itemsToShow: 3 },
+    // { width: 5, itemsToShow: 4 }
   ];
 
 class Slider extends Component{
     state = {
-        imageUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQenFLGsLXa2WrfKCYcxHh_ER-kcoW6-Pywgg&usqp=CAU"
+        usersProfilePictures: []
     };
 
     styles = {
-        width:200
+        width:100,
+        height: 100
     };
 
+    componentDidMount(){
+      axios.get('https://jsonplaceholder.typicode.com/photos').then(res => {
+        console.log(res);
+        this.setState({usersProfilePictures: res.data})
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+
       render(){
+       const {usersProfilePictures} = this.state
+
             return (
 
                 <div className="carousel-container">
                 <Carousel breakPoints={breakPoints}>
-                  <Item>
-                      <img style={this.styles} src={this.state.imageUrl} alt="test"></img>
-                  </Item>
-                  <Item>2</Item>
-                  <Item>3</Item>
-                  <Item>4</Item>
-                  <Item>5</Item>
-                  <Item>6</Item>
-                  <Item>7</Item>
-                  <Item>8</Item>
-                  <Item>9</Item>
-                  <Item>10</Item>
+
+                {usersProfilePictures.length ? usersProfilePictures.map(image => <Item key={image.id}><img style={this.styles} src={image.url} alt="test"></img></Item>): null}
                 </Carousel>
               </div>
                 );
