6fd12bd55c57280569d5e4686b9fddc5e7aed12e
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-06 00:34:20 +0100

    updated slider style

--- front-end-codeship/src/components/Item.js
+++ front-end-codeship/src/components/Item.js
@@ -4,10 +4,10 @@ export default styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100px;
-  width: 100px;
+  height: 50px;
+  width: 50px;
   background-color: #00008B;
   color: #fff;
-  margin: 0 5px;
+  margin-right: 20px;
   font-size: 4em;
 `;
