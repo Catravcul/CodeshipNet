5ff90884e205ac14c991dd98ed5ae59872e82331
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-06 01:00:09 +0100

    added form and nav buttons components

--- front-end-codeship/src/pages/ProfileContainer.js
+++ front-end-codeship/src/pages/ProfileContainer.js
@@ -2,6 +2,8 @@ import React from "react";
 
 //Profile Components 
 import Slider from "../components/Slider"
+import Form from "../components/Form"
+import ButtonsNav from "../components/ButtonsNav"
 
 function ProfileContainer(){
     return(
@@ -12,9 +14,12 @@ function ProfileContainer(){
 
               </div>
               <div className="navbar">
+                  <ButtonsNav></ButtonsNav>
+                  <ButtonsNav></ButtonsNav>
+                  <ButtonsNav></ButtonsNav>
+
 
             </div>
-
           </div>
           <div className="profile-info-container">
               <div className="users-container">
@@ -23,7 +28,7 @@ function ProfileContainer(){
               </div>
               <div className="profile-spaceship-info">
                   <div className="profile-spaceship-details">
-
+                      <Form></Form>
                   </div>
                   <div className="user-spaceship-details">
                       <div className="user-spaceship-link-container">
@@ -33,12 +38,10 @@ function ProfileContainer(){
                           <div className="image-container">
 
                           </div>
-
                       </div>
                       <div className="products-slider-container">
-
+                          <Slider></Slider>
                       </div>
-                      
                   </div>
 
               </div>
