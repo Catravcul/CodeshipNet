491207750eb43c18f78a41e2acaa9cc68aa133b4
Carlota Gallart <carlota.gallart@gmail.com>
2020-12-07 19:04:22 +0100

    ShopContainer.js - fetch products, map with props

--- front-end-codeship/src/pages/ShopContainer.js
+++ front-end-codeship/src/pages/ShopContainer.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 // import components
-import ProductCard from '../components/ProductCard'
-import ShoppingCart from '../components/ShoppingCart'
-import TopSpaceShip from '../components/TopSpaceShip'
-function ShopContainer(){
-return(
-<div>ShopContainer
-<ProductCard></ProductCard>
-<TopSpaceShip></TopSpaceShip>
-<ShoppingCart></ShoppingCart>
-</div>
+import SelectProduct from "../components/SelectProduct";
+import ProductCard from "../components/ProductCard";
+import ShoppingCart from "../components/ShoppingCart";
+import TopSpaceShip from "../components/TopSpaceShip";
+// import ProductDetail from '../components/ProductDetail'
 
-)
+function ShopContainer() {
+  const [products, setProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    fetch("https://codeship-api.herokuapp.com/product", { method: "GET" })
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setProducts([...data.products]);
+        setAllProducts([...data.products])
+        //  console.log(products)
+      });
+  }, []);
+
+  return (
+    <div className="ShopContainer">
+      <div className="LeftContainer">
+        <SelectProduct allProducts={allProducts} setProducts={setProducts}></SelectProduct>
+        <div className="ProductCardContainer">
+          {products.map((product) => (
+            <ProductCard product={product} cart={cart} products={products} setCart={setCart}></ProductCard>
+          ))}
+        </div>
+      </div>
+      <div className="RightContainer">
+        <TopSpaceShip></TopSpaceShip>
+        <ShoppingCart cart={cart} setCart={setCart}></ShoppingCart>
+      </div>
+    </div>
+  );
 }
 export default ShopContainer;
