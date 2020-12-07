be1f2cd6547b47ffffc941928ebb2cd68e7b7224
Carlota Gallart <carlota.gallart@gmail.com>
2020-12-07 18:58:57 +0100

    ShoppingCart.js - ShoppingCart structure HTML, deleteProduct event, TotalPrice function and map by props

--- front-end-codeship/src/components/ShoppingCart.js
+++ front-end-codeship/src/components/ShoppingCart.js
@@ -1,12 +1,42 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faMinusCircle} from "@fortawesome/free-solid-svg-icons"
+function ShoppingCart(props) {
 
-function ShoppingCart() {
+  const deleteProduct = (id) =>{
+    const productIndex = props.cart.findIndex(product => product._id === id)
+    let cart = props.cart
+    cart.splice(productIndex, 1)
+    props.setCart([...cart])
+  }
+
+  const getTotalPrice = () =>{
+    let result = 0;
+    props.cart.forEach(product => {
+      result += product.price
+    });
+    return result;
+  }
   return (
-    <div>
+    <div className="ShoppingCartContainer">
         <h1>CART</h1>
-        <img src="http://www.pngmart.com/files/3/Spaceship-PNG-Photo.png"/>
-        <h3>Motor Spaceship</h3>
-        <p>503 pt</p>
+        {props.cart.map((product) =>(
+        <div className="ShoppingProducts">
+        <img src={"https://codeship-api.herokuapp.com/" + product.img_path}/>
+          <div className="ShoppingProductsInfo">
+            <h3>{product.title}</h3>
+            <p>{product.price} points</p>
+          </div>
+          <FontAwesomeIcon icon={faMinusCircle} onClick={()=> deleteProduct(product._id)} className="icon" />
+        </div>
+        ))}
+        <div className="TotalProducts">
+          <h3>Total:</h3>
+          <p>&nbsp;{getTotalPrice()}  points</p>
+        </div>
+        <div className="BuyButton">
+          <button>BUY</button>
+        </div>
     </div>
   );
 }
