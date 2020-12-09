a7618e7c6e1d52385d748271268d33b24152b4c1
Carlota Gallart <carlota.gallart@gmail.com>
2020-12-09 20:41:06 +0100

    ShoppingCart.js - modal coins added

--- front-end-codeship/src/components/ShoppingCart.js
+++ front-end-codeship/src/components/ShoppingCart.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, {useState}  from "react";
+import Modal from "react-modal";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faMinusCircle} from "@fortawesome/free-solid-svg-icons"
 function ShoppingCart(props) {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "none",
+      backgroundColor: "black",
+      border: "none",
+      inset: "10% auto auto 50%",
+    },
+  };
 
   const deleteProduct = (id) =>{
     const productIndex = props.cart.findIndex(product => product._id === id)
@@ -25,18 +42,49 @@ function ShoppingCart(props) {
         <img src={"https://codeship-api.herokuapp.com/" + product.img_path}/>
           <div className="ShoppingProductsInfo">
             <h3>{product.title}</h3>
-            <p>{product.price} points</p>
+            <p>{product.price} coins</p>
           </div>
           <FontAwesomeIcon icon={faMinusCircle} onClick={()=> deleteProduct(product._id)} className="icon" />
         </div>
         ))}
         <div className="TotalProducts">
           <h3>Total:</h3>
-          <p>&nbsp;{getTotalPrice()}  points</p>
+          <p>&nbsp;{getTotalPrice()}  coins</p>
         </div>
         <div className="BuyButton">
-          <button>BUY</button>
+          <button onClick={() => setModalIsOpen(true)}>BUY</button>
         </div>
+        <Modal style={customStyles} className="modal" isOpen={modalIsOpen}>
+            <span className="closeModal" onClick={() => setModalIsOpen(false)}>
+              X
+            </span>
+            <div className="CoinsContainer">
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>10 COINS</p>
+              </div>
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>50 COINS</p>
+              </div>
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>75 COINS</p>
+              </div>
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>100 COINS</p>
+              </div>
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>250 COINS</p>
+              </div>
+              <div className="Coins">
+              <img src="https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"></img>
+              <p>500 COINS</p>
+              </div>
+            </div>
+          </Modal>
     </div>
   );
 }
