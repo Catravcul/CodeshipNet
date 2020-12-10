4de89b27be68c25e189baac8a4f6ce67ff3cc9c0
Iman Aazibou Echatbi <imansharkie@Imans-MacBook-Air.local>
2020-12-10 14:25:15 +0100

    login section html structure

---
+++ front-end-codeship/src/pages/LoginContainer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+//login components
+import LoginForm from "../components/LoginForm"
+
+function LoginContainer(){
+    return(<div className="register-and-login-container">
+        <div className="form-container">
+            <div className="image-design-login">
+            </div>
+            <div className="login-form-container">
+                <LoginForm></LoginForm>
+
+            </div>
+        </div>
+
+    </div>)
+
+}
+
+export default LoginContainer;
