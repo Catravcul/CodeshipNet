6f0a01ccc5f04e01a5ddbea5499ff55eb291cba1
Iman Aazibou Echatbi <imansharkie@Imans-MacBook-Air.local>
2020-12-10 14:24:45 +0100

    removed extra button

--- front-end-codeship/src/pages/ProfileContainer.js
+++ front-end-codeship/src/pages/ProfileContainer.js
@@ -16,42 +16,32 @@ function ProfileContainer(){
               <div className="navbar">
                   <ButtonsNav title="Profile"></ButtonsNav>
                   <ButtonsNav title="Spaceship"></ButtonsNav>
-                  <ButtonsNav></ButtonsNav>
-
-
             </div>
           </div>
           <div className="profile-info-container">
               <div className="users-container">
                   {/* <Slider></Slider> */}
-
               </div>
               <div className="profile-spaceship-info">
                   <div className="profile-spaceship-details">
-                      
                       <Form update={true}></Form>
-                    
                   </div>
                   <div className="user-spaceship-details">
                       <div className="user-spaceship-link-container">
-
                       </div>
                       <div className="user-spaceship-container">
                           <div className="image-container">
-
                           </div>
                       </div>
                       <div className="products-slider-container">
                           {/* <Slider></Slider> */}
                       </div>
                   </div>
-
               </div>
-
           </div>
-
       </div>
   </div>
     )
     }
-    export default ProfileContainer;
+
+export default ProfileContainer;
