85d0ad706544c4d8d6e97b9274347ed38d47477e
Carlota Gallart <carlota.gallart@gmail.com>
2020-12-10 16:08:20 +0100

    Header.js - modal logout and profile finished

--- front-end-codeship/src/components/Header.js
+++ front-end-codeship/src/components/Header.js
@@ -3,8 +3,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faChevronDown} from "@fortawesome/free-solid-svg-icons"
 function Header(){
 var [toggle, setToggle] = useState(false)
-toggle = () => {
-    console.log('hola')
+let userModal
+if(toggle){
+    userModal = 
+    <div className="ModalProfileSignOut">
+   <a href="/profile">Profile</a>
+   <a href="/login">Sign Out</a>
+</div>
 }
     return(
         <header>
@@ -15,12 +20,9 @@ toggle = () => {
                     <p>500 coins</p>
                     </div>
                     <img src="https://i.scdn.co/image/9ed3f6a2f42b970bcfcab147a50ad22243e2d1a5"></img>
-                    <FontAwesomeIcon icon={faChevronDown}  onClick={() => setToggle(true)} className="icon" />
+                    <FontAwesomeIcon icon={faChevronDown}  onClick={() => setToggle(!toggle)} className="icon" />
+                    {userModal}
                 </div>
-                <div className="ModalProfileSignOut" isOpen={toggle}>
-               <a href="/profile">Profile</a>
-               <a href="/login">Sign Out</a>
-           </div>
 
         </header>
     )
