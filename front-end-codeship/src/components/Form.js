3e8796b2eebff93be0fc3cf4faf1d2753b4f0427
Iman Aazibou Echatbi <imansharkie@Imans-MacBook-Air.local>
2020-12-10 12:19:39 +0100

    form validation

--- front-end-codeship/src/components/Form.js
+++ front-end-codeship/src/components/Form.js
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import axios from 'axios';
 // import { useForm } from "react-hook-form";
 
+// username, lastname, email, password, passwordConfirm, description, image and points
 const initialState = {
-  userName: "",
-  userUsername: "",
-  userPassword: "",
-  nameError: "",
-  usernameError: "",
-  passwordError: ""
+  username: "",
+  name: "",
+  lastname: "",
+  email: "",
+  password: "",
+  passwordConfirm: "",
+  description: "",
+  image: "",
+  errors: []
 }
 
 class Form extends Component {
-
 
     state= initialState;
 
@@ -23,15 +26,50 @@ class Form extends Component {
 
   //form validation
   validate = () => {
-    let nameError= "";
+    let errors= [];
  
-    if(this.state.userName.includes('$')){
-      nameError = "can't include the $ sign";
+    if(this.state.username.length < 3){
+      errors[0] = "Username should not be shorter than 3 characters";
     }
-    if(nameError){
-      this.setState({ nameError });
+    if(this.state.username.length > 15){
+      errors[0] = "Username should not be longer than 15 characters";
+    }
+    if(this.state.name.length < 3){
+      errors[1] = "Name should not be shorter than 12 characters";
+    }
+    if(this.state.name.length > 12){
+      errors[1] = "Name should not be shorter than 12 characters";
+    }
+
+    if(this.state.lastname.length < 3){
+      errors[2] = "Lastname should not be shorter than 3 characters";
+    }
+
+    if(this.state.lastname.length > 15){
+      errors[2] = "Lastname should not be longer than 15 characters";
+    }
+
+    if(!this.state.email.includes('@')){
+      errors[3] = "Mail should have an email format '@' "
+    }
+
+    if(this.state.password < 6){
+      errors[4] = "Password should be longer than 6 characters";
+    }
+
+    if(this.state.passwordConfirm !== this.state.password){
+      errors[5] = "Password does not match";
+    }
+
+    if(this.state.description < 250){
+      errors[6] = "Description can't be longer than 250 characters"
+    }
+
+    if(errors){
+      this.setState({errors });
       return false;
     }
+
  return true;
   };
 
@@ -73,7 +111,7 @@ class Form extends Component {
 
  render(){
 
-  const {userName, userUsername, userPassword} = this.state
+  const {username, name, lastname, email, password, passwordConfirm, description, image} = this.state
 
   //register/update button display
   let myButton;
@@ -82,38 +120,58 @@ class Form extends Component {
   } else {
     myButton = <button type="submit"> Update </button>}
   
-
-
+  let button
   return (
 
-        <div className="form-container">
+    <form class="form-container" onSubmit={this.props.update?this.submitUpdate:this.submitHandler}>
             <div className="actual-form">
             <div className="image-register-container">
-              <p>hgfghv</p>
+            <div>
+                  <label for="image">Add Image</label>
+                  <input type="image" id="image" name="image" value={image} onChange={this.changeHandler}placeholder="Upload a profile picture"></input>
+                </div>
+                <div>{this.state.imageError}</div>
             </div>
             <div className="form-register-container">
-            <form onSubmit={this.props.update?this.submitUpdate:this.submitHandler}>
-                <div>
-                  <label for="userName">Name</label>
-                  <input type="text" id="userName" name="userName" value={userName} onChange={this.changeHandler}placeholder="write your name"></input>
+            <div>
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" value={username} onChange={this.changeHandler}placeholder="write your name"></input>
                 </div>
-               <div>{this.state.nameError}</div>
-                <div>
-                  <label for="userUsername">Username</label>
-                  <input type="text" id="userUsername" name="userUsername" value={userUsername} onChange={this.changeHandler}placeholder="write a username"></input>
+               <div>{this.state.errors[0]}</div>
+               <div>
+                  <label for="name"> Name</label>
+                  <input type="text" id="name" name="name" value={name} onChange={this.changeHandler}placeholder="write your name"></input>
                 </div>
-                <div>{this.state.usernameError}</div>
+                <div>{this.state.errors[1]}</div>
                 <div>
-                  <label for="userPassword">Password</label>
-                  <input type="text" id="userPassword" name="userPassword" value={userPassword} onChange={this.changeHandler}placeholder="write a password"></input>
+                  <label for="lastname"> Last name</label>
+                  <input type="text" id="lastname" name="lastname" value={lastname} onChange={this.changeHandler}placeholder="write your last name"></input>
                 </div>
-                <div>{this.state.passwordError}</div>
-                {/* <button type="submit"></button> */}
+                <div>{this.state.errors[2]}</div>
+                <div>
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" value={email} onChange={this.changeHandler}placeholder="write your email"></input>
+                </div>
+                <div>{this.state.errors[3]}</div>
+                <div>
+                  <label for="password">Password</label>
+                  <input type="text" id="password" name="password" value={password} onChange={this.changeHandler}placeholder="write a password"></input>
+                </div>
+                <div>{this.state.errors[4]}</div>
+                <div>
+                  <label for="passwordConfirm">Confirm Password</label>
+                  <input type="text" id="passwordConfirm" name="passwordConfirm" value={passwordConfirm} onChange={this.changeHandler}placeholder=" confirm password"></input>
+                </div>
+                <div>{this.state.errors[5]}</div>
+                <div>
+                  <label for="description">Description</label>
+                  <input type="text" id="description" name="description" value={description} onChange={this.changeHandler}placeholder="write a description"></input>
+                </div>
+                <div>{this.state.descriptionError}</div>
                 {myButton}
-              </form>
             </div>
-            </div>
-        </div>
+          </div>
+        </form>
   );
 }
 }
