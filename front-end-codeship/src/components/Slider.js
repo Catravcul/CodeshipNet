0e615017cf79146aa4a5127f9731ace72b645e9e
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-11 13:51:43 +0100

    removed fetches

--- front-end-codeship/src/components/Slider.js
+++ front-end-codeship/src/components/Slider.js
@@ -11,60 +11,22 @@ const breakPoints = [
   ];
 
 class Slider extends Component{
-    state = {
-        usersProfilePictures: []
-    };
+    // state = {
+    //     usersProfilePictures: []
+    // };
 
     styles = {
         width:100,
         height: 100
     };
 
-
-    
- 
-
-    //fetch users
-    componentDidMount(){
-
-    const token = sessionStorage.getItem("codeship-token")
-      fetch('https://codeship-api.herokuapp.com/user', {method: "GET", headers:{"x-access-token": token}})
-      .then(res => {
-        return res.json();
-      })
-      .then(data =>{
-        console.log(data)
-        // this.setState({usersProfilePictures: products.data.products})
-      })
-      .catch(error => {
-        console.log(error)
-      })
-
-     //fetch products
-      fetch('https://codeship-api.herokuapp.com/public/product', {method: "GET"})
-
-      .then(res => {
-        return res.json();
-      })
-      .then(products =>{
-        console.log(products)
-        this.setState({usersProfilePictures: products.data.products})
-      })
-      .catch(error => {
-        console.log(error)
-      })
-    }
-
       render(){
-       const {usersProfilePictures} = this.state
-       console.log(this.state);
-
             return (
 
                 <div className="carousel-container">
                 <Carousel breakPoints={breakPoints}>
 
-                {usersProfilePictures.length ? usersProfilePictures.map(image => <Item key={image.id}><img style={this.styles} src={"https://codeship-api.herokuapp.com/" + image.img_path} alt={image.title}></img></Item>): null}
+                {this.props.items.length ? this.props.items.map(image => <Item key={image.id}><img style={this.styles} src={"https://codeship-api.herokuapp.com/" + image.img_path} alt={image.title}></img></Item>): null}
                 </Carousel>
               </div>
                 );
