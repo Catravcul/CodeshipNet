f6e96e47e582296c319dd65bdf0123a4ad739952
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-11 12:38:31 +0100

    fetch for users and products

--- front-end-codeship/src/components/Slider.js
+++ front-end-codeship/src/components/Slider.js
@@ -4,7 +4,7 @@ import Item from "./Item";
 // import axios from "axios";
 
 const breakPoints = [
-    { width: 1, itemsToShow: 8 }
+    { width: 1, itemsToShow: 6 }
     // { width: 3, itemsToShow: 2, itemsToScroll: 2 },
     // { width: 4, itemsToShow: 3 },
     // { width: 5, itemsToShow: 4 }
@@ -20,8 +20,27 @@ class Slider extends Component{
         height: 100
     };
 
+
+    
+ 
+
+    //fetch users
     componentDidMount(){
 
+    const token = sessionStorage.getItem("codeship-token")
+      fetch('https://codeship-api.herokuapp.com/user', {method: "GET", headers:{"x-access-token": token}})
+      .then(res => {
+        return res.json();
+      })
+      .then(data =>{
+        console.log(data)
+        // this.setState({usersProfilePictures: products.data.products})
+      })
+      .catch(error => {
+        console.log(error)
+      })
+
+     //fetch products
       fetch('https://codeship-api.herokuapp.com/public/product', {method: "GET"})
 
       .then(res => {
@@ -38,17 +57,20 @@ class Slider extends Component{
 
       render(){
        const {usersProfilePictures} = this.state
+       console.log(this.state);
 
             return (
 
                 <div className="carousel-container">
                 <Carousel breakPoints={breakPoints}>
 
-                {usersProfilePictures.length ? usersProfilePictures.map(image => <Item key={image.id}><img style={this.styles} src={image.img_path} alt="test"></img></Item>): null}
+                {usersProfilePictures.length ? usersProfilePictures.map(image => <Item key={image.id}><img style={this.styles} src={"https://codeship-api.herokuapp.com/" + image.img_path} alt={image.title}></img></Item>): null}
                 </Carousel>
               </div>
                 );
              }
       }
+
+      
 export default Slider;
 
