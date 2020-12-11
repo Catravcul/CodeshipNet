8f7abc5cc7c25f7d0981d2c47aff5f158baaf039
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-11 13:51:12 +0100

    nav btns event, fetch users and products

--- front-end-codeship/src/pages/ProfileContainer.js
+++ front-end-codeship/src/pages/ProfileContainer.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //Profile Components 
 import Slider from "../components/Slider"
 import Form from "../components/Form"
 import ButtonsNav from "../components/ButtonsNav"
 
+
+
 function ProfileContainer(){
+
+    const [usersProfilePictures, setUsersProfilePictures] = useState([])
+    const [productsImages, setProductsImages] = useState([])
+
+    //fetch users
+    useEffect(() => { 
+
+        const token = sessionStorage.getItem("codeship-token")
+          fetch('https://codeship-api.herokuapp.com/user', {method: "GET", headers:{"x-access-token": token}})
+          .then(res => {
+            return res.json();
+          })
+          .then(data =>{
+            console.log(data)
+            setUsersProfilePictures(data.data.users)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+    
+         //fetch products
+          fetch('https://codeship-api.herokuapp.com/public/product', {method: "GET"})
+    
+          .then(res => {
+            return res.json();
+          })
+          .then(products =>{
+            console.log(products)
+            setProductsImages(products.data.products)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+        },[])
+    
+        //events for profile and spaceship update
+        
+        const showUpdateProfile = () =>{ 
+            console.log("profile")
+        }
+
+        const showUpdateSpaceship = () =>{ 
+            console.log("spaceship")
+        }
+
     return(
   <div className="profile-container">
       <div className="profile-content">
@@ -14,13 +61,13 @@ function ProfileContainer(){
 
               </div>
               <div className="navbar">
-                  <ButtonsNav title="Profile"></ButtonsNav>
-                  <ButtonsNav title="Spaceship"></ButtonsNav>
+                  <ButtonsNav title="Profile" click={showUpdateProfile}></ButtonsNav>
+                  <ButtonsNav title="Spaceship" click={showUpdateSpaceship}></ButtonsNav>
             </div>
           </div>
           <div className="profile-info-container">
               <div className="users-container">
-                  {/* <Slider></Slider> */}
+                  <Slider items={usersProfilePictures}></Slider>
               </div>
               <div className="profile-spaceship-info">
                   <div className="profile-spaceship-details">
@@ -34,7 +81,7 @@ function ProfileContainer(){
                           </div>
                       </div>
                       <div className="products-slider-container">
-                          <Slider></Slider>
+                          <Slider items={productsImages}></Slider>
                       </div>
                   </div>
               </div>
