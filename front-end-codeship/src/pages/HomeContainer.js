916b77ddf4a5a642f349ef12bd45da71dc675fff
carlota <carlota.gallart@gmail.com>
2020-12-22 14:40:49 +0100

    HomeContainer.js -  structure home container

--- front-end-codeship/src/pages/HomeContainer.js
+++ front-end-codeship/src/pages/HomeContainer.js
@@ -1,6 +1,14 @@
-function HomeContainer(){
-    return(
-    <div>HomeContainer</div>
-    )
-    }
-    export default HomeContainer;
+import React from "react";
+import { Link } from "react-router-dom";
+function HomeContainer() {
+  return (
+    <div className="HomeContainer">
+      <div className="ProfileHome">PROFILE</div>
+      <div className="ProfilePlay">PLAY</div>
+      <div className="ProfileShop">
+        <Link to="/shop">Shop</Link>
+      </div>
+    </div>
+  );
+}
+export default HomeContainer;
