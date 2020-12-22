6e3d06f6d3308cd8848f4ff640f90690df134c67
carlota <carlota.gallart@gmail.com>
2020-12-22 14:39:16 +0100

    App.js - import and route home

--- front-end-codeship/src/App.js
+++ front-end-codeship/src/App.js
@@ -1,27 +1,26 @@
-import React from 'react'
+import React from "react";
 
 // import react-router-dom without BrowserRouter gives route errors
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 // import pages
-import ShopContainer from './pages/ShopContainer'
-// import HomeContainer from './pages/HomeContainer'
+import ShopContainer from "./pages/ShopContainer";
+import HomeContainer from "./pages/HomeContainer";
 
 //import route path
 import ROUTES from "./utils/routes";
 
-
 function App() {
   return (
     <BrowserRouter>
-    <Switch>
-      {/* <Route path={ROUTES.HOME} exact>
-        <HomeContainer />
-      </Route> */}
-      <Route path={ROUTES.SHOP} exact>
-        <ShopContainer />
-      </Route>
-    </Switch>
+      <Switch>
+        <Route path={ROUTES.HOME} exact>
+          <HomeContainer />
+        </Route>
+        <Route path={ROUTES.SHOP} exact>
+          <ShopContainer />
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
