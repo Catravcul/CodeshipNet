e9357a7cf962c383e2c7d4d8ccbcce005415dd7b
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-23 16:02:02 +0100

    props set session

--- front-end-codeship/src/pages/LoginContainer.js
+++ front-end-codeship/src/pages/LoginContainer.js
@@ -9,7 +9,7 @@ function LoginContainer(props){
             <div className="image-design-login">
             </div>
             <div className="login-form-container">
-                <LoginForm token={props.token}></LoginForm>
+                <LoginForm token={props.token} setSession={props.setSession}></LoginForm>
 
             </div>
         </div>
