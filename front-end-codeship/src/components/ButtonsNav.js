4b15cb1dea3f64450178f5ad2a5c815414beb2d0
Iman Aazibou Echatbi <imansharkie@Imans-Air.home>
2020-12-23 13:29:35 +0100

    profile nav btns title

--- front-end-codeship/src/components/ButtonsNav.js
+++ front-end-codeship/src/components/ButtonsNav.js
@@ -3,7 +3,7 @@ import React, {Component} from "react";
 function NavButton(props){
     return(
     <div className="nav-buttons-container">
-            <button type="button" onClick={props.click} title={props.title}> </button>
+            <button type="button" onClick={props.click} >{props.title}</button>
     </div>)
 }
 
