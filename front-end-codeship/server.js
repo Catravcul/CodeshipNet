e6d763023024fc1e8c4e197f5e5617732f83220d
carlota <carlota.gallart@gmail.com>
2020-12-28 12:16:07 +0100

    server.js - cors implemented for codeship game

--- front-end-codeship/server.js
+++ front-end-codeship/server.js
@@ -1,11 +1,18 @@
-const express = require('express');
-require('dotenv').config()
-const route = express()
-route.use(express.static(__dirname + '/build'));
-console.log(__dirname)
-route.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/build/index.html')
+const express = require("express");
+require("dotenv").config();
+const route = express();
+route.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://codeship-game.herokuapp.com"
+  );
+  res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
+  next();
 });
-route.listen(3000, ()=>{
-    console.log('http://localhost:3000/')
-})
+route.use(express.static(__dirname + "/build"));
+route.get("/", (req, res) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
+route.listen(3000, () => {
+  console.log("http://localhost:3000/");
+});
