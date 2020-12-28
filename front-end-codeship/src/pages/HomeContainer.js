80767b5927df164becff9ac316d7b0ffabafb864
carlota <carlota.gallart@gmail.com>
2020-12-28 12:21:34 +0100

    HomeContainer.js - add react links [profile, shop] and href to the game

--- front-end-codeship/src/pages/HomeContainer.js
+++ front-end-codeship/src/pages/HomeContainer.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 function HomeContainer() {
   return (
     <div className="HomeContainer">
-      <div className="ProfileHome">PROFILE</div>
-      <div className="ProfilePlay">PLAY</div>
-      <div className="ProfileShop">
-        <Link to="/shop">Shop</Link>
+      <div className="FlexboxHome">
+        <div className="ProfileHome">
+          <Link to="/profile">
+            <h1>PROFILE</h1>
+          </Link>
+        </div>
+        <div className="PlayHome">
+          <a href="https://codeship-game.herokuapp.com/">
+            <h1>PLAY</h1>
+          </a>
+        </div>
+        <div className="ShopHome">
+          <Link to="/shop">
+            <h1>SHOP</h1>
+          </Link>
+        </div>
       </div>
     </div>
   );
