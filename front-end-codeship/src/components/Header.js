f7867cba7811b0c47b6ea5775d7a08ad4b24eaa0
carlota <carlota.gallart@gmail.com>
2020-12-28 15:58:09 +0100

    Header.js - add session to header

--- front-end-codeship/src/components/Header.js
+++ front-end-codeship/src/components/Header.js
@@ -1,31 +1,37 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronDown} from "@fortawesome/free-solid-svg-icons"
-function Header(){
-var [toggle, setToggle] = useState(false)
-let userModal
-if(toggle){
-    userModal = 
-    <div className="ModalProfileSignOut">
-   <a href="/profile">Profile</a>
-   <a href="/login">Sign Out</a>
-</div>
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+function Header(props) {
+  var [toggle, setToggle] = useState(false);
+  let userModal;
+  if (toggle) {
+    userModal = (
+      <div className="ModalProfileSignOut">
+        <a href="/profile">Profile</a>
+        <a href="/login">Sign Out</a>
+      </div>
+    );
+  }
+  return (
+    <header>
+      <h1>CODESHIP</h1>
+      <div className="infoProfileHeader">
+        <div className="infoUserHeader">
+          <h4>{props.session.username}</h4>
+          <p>{props.session.points} coins</p>
+        </div>
+        <img
+          src={"https://codeship-api.herokuapp.com/" + props.session.img_path}
+        ></img>
+        <FontAwesomeIcon
+          icon={faChevronDown}
+          onClick={() => setToggle(!toggle)}
+          className="icon"
+        />
+        {userModal}
+      </div>
+    </header>
+  );
 }
-    return(
-        <header>
-                <h1>CODESHIP</h1>
-                <div className="infoProfileHeader">
-                    <div className="infoUserHeader">
-                    <h4>username_01</h4>
-                    <p>500 coins</p>
-                    </div>
-                    <img src="https://i.scdn.co/image/9ed3f6a2f42b970bcfcab147a50ad22243e2d1a5"></img>
-                    <FontAwesomeIcon icon={faChevronDown}  onClick={() => setToggle(!toggle)} className="icon" />
-                    {userModal}
-                </div>
 
-        </header>
-    )
-}
-
-export default Header
+export default Header;
