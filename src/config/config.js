4f9eb8d003d853955e257765cadc71842be38347
beron <bleink@outlook.es>
2020-12-31 12:59:08 +0100

    config.js - drop last consts, add [codeshipApi, codeshipGame] objects with urlBase property, rename func to get consts to getConfig

--- src/config/config.js
+++ src/config/config.js
@@ -1,36 +1,36 @@
 const mode = 'PROD'
-// import "dotenv/config";
-//  console.log(process.env)
+
 const development = {
- spaceship: {
- urlBASE : 'http://localhost:5000',
- login : '/auth/login',
- signup: '/signup'
- }
- }
- const test = {
- spaceship: {
- urlBASE : 'http://localhost:5000',
- login : '/auth/login',
- signup: '/signup'
- }
- }
- const production = {
- spaceship: {
- urlBASE : 'https://codeship-api.herokuapp.com',
- login : '/auth/login',
- signup: '/signup',
- product: '/product'
- }
- }
- 
-export function getSpaceshipConfig() {
- switch(mode) {
- case "DEV" :
- return development;
- case "TEST" :
- return test;
- default :
- return production;
- }
+    codeshipApi: {
+        urlBase: 'http://127.0.0.1:5000'
+    }, 
+    codeshipGame: {
+        urlBase: 'http://127.0.0.1:3000'
+    }
 }
+const test = {
+    codeshipApi: {
+        urlBase: 'http://127.0.0.1:5000'
+    }, 
+    codeshipGame: {
+        urlBase: 'http://127.0.0.1:3000'
+    }
+}
+const production = {
+    codeshipApi: {
+        urlBase: 'https://codeship-api.herokuapp.com'
+    }, 
+    codeshipGame: {
+        urlBase: 'https://codeship-game.herokuapp.com'
+    }
+}
+    
+export function getConfig() {
+    switch(mode) {
+    case "DEV" :
+    return development;
+    case "TEST" :
+    return test;
+    default :
+    return production;
+}}
